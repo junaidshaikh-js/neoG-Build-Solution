1a178c6f8d8f935a90f925ec003b641b25ba8424
Junaid <sj.shaikhjunaid@gmail.com>
2022-01-11 13:10:57 +0530

    solve: star pattern

---
+++ operator-branching-loop/intermediate/two.js
@@ -0,0 +1,23 @@
+/* Print the following star pattern :-
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+
+function printStarPattern() {
+  for (let i = 0; i < 5; i++) {
+    let str = "";
+
+    for (let j = 0; j <= i; j++) {
+      str += "*";
+    }
+
+    console.log(str);
+  }
+}
+
+printStarPattern();
