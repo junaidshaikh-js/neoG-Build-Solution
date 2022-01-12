57119a3dabb5eacad284d202037848b1d16d9778
Junaid <sj.shaikhjunaid@gmail.com>
2022-01-12 13:20:29 +0530

    solve: power

---
+++ functions/easy/one.js
@@ -0,0 +1,13 @@
+/* 
+
+Given a and b, your function should return the value of ab
+Example:
+Input: power(2,3) ––> Output: 8
+
+*/
+
+function power(a, b) {
+  return a ** b;
+}
+
+console.log(power(2, 3));
